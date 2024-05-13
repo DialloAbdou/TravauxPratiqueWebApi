2f1387e3ded7b8b462ca157f67d48be2235b2e1e
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-05-13 20:34:49 +0200

    Creation de la classe Endpoint

---
+++ Tp2/TpWebApi/WebApiTodoList/EndPoinds/TodoEnpoints.cs
@@ -0,0 +1,120 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+using WebApiTodoList.Dto;
+using WebApiTodoList.services;
+
+namespace WebApiTodoList.EndPoinds
+{
+    public static class TodoEnpoints
+    {
+        // prefice: todo
+        public static RouteGroupBuilder MapTodoEndPoint(this RouteGroupBuilder group)
+        {
+            group.MapGet("", GetAll);
+
+            group.MapGet("/{id:int}", GetById);
+
+            group.MapPost("", AddTodoAsync);
+
+            group.MapPut("/{id:int}", UpdateAsync);
+
+            group.MapDelete("/{id:int}", DeleteAsync);
+
+
+            return group;
+        }
+
+        /// <summary>
+        ///  elle renvoie la liste des elements 
+        /// </summary>
+        /// <param name="_todoservice"></param>
+        /// <returns></returns>
+        private static async Task<IResult> GetAll(
+            [FromServices] ITodoService _todoservice)
+        {
+            var _todos = await _todoservice.GetAllTodoAsync();
+            return Results.Ok(_todos);
+        }
+
+        /// <summary>
+        /// elle renvoie un todo
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="service"></param>
+        /// <returns></returns>
+        private static async Task<IResult> GetById(
+            [FromRoute] int id,
+            [FromServices] ITodoService service)
+        {
+            var _todo = await service.GetTodoByIDAsync(id);
+            if (_todo is not null) return Results.Ok(_todo);
+            return Results.NotFound();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="input"></param>
+        /// <param name="validator"></param>
+        /// <param name="service"></param>
+        /// <returns></returns>
+        private static async Task<IResult> AddTodoAsync(
+                [FromBody] TodoInput input,
+                [FromServices] IValidator<TodoInput> validator,
+                [FromServices] ITodoService service)
+        {
+            var result = validator.Validate(input);
+            if (!result.IsValid)
+            {
+                return Results.BadRequest(result.Errors.Select(e => new
+                {
+                    e.ErrorMessage,
+                    e.PropertyName
+                }));
+            }
+            var _todo = await service.AddTodoAsync(input);
+
+            return Results.Ok(_todo);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="input"></param>
+        /// <param name="validator"></param>
+        /// <param name="service"></param>
+        /// <returns></returns>
+
+        private static async Task<IResult> UpdateAsync(
+            [FromRoute] int id,
+            [FromBody] TodoInput input,
+            [FromServices] IValidator<TodoInput> validator,
+            [FromServices] ITodoService service)
+        {
+            var result = validator.Validate(input);
+            if (!result.IsValid)
+            {
+                return Results.BadRequest(result.Errors.Select(e => new
+                {
+                    e.ErrorMessage,
+                    e.PropertyName
+                }));
+            }
+            var _todo = await service.UpdateTodoAsync(id, input);
+            return Results.Ok(_todo);
+        }
+
+        private static async Task<IResult> DeleteAsync(
+            [FromRoute] int id,
+            [FromServices] ITodoService service)
+        {
+            var result = await service.DeleteTodoAsync(id);
+            if (result) return Results.Content("Supprimé!");
+            return Results.NoContent();
+        }
+    }
+}
--- Tp2/TpWebApi/WebApiTodoList/Program.cs
+++ Tp2/TpWebApi/WebApiTodoList/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 using WebApiTodoList.Data;
 using WebApiTodoList.Dto;
+using WebApiTodoList.EndPoinds;
 using WebApiTodoList.services;
 
 var builder = WebApplication.CreateBuilder();
@@ -30,6 +31,9 @@ builder.Services.AddScoped< ITodoService, TodoService> ();
 
 var app = builder.Build();
 
+app.MapGroup("/todo")
+    .MapTodoEndPoint();
+
 //await app.Services
 //    .CreateAsyncScope()
 //    .ServiceProvider
@@ -41,63 +45,63 @@ await app.Services
     .GetRequiredService<TodoDbContext>()
     .Database.MigrateAsync();
 
-app.MapGet("/todo",  async( [FromServices] ITodoService _todoservice,ILogger<Program> loger) =>
-{
-    var  _todos = await _todoservice.GetAllTodoAsync();
-    loger.LogInformation("Hello Abdou Bienvenu sur les loggers");
-    return Results.Ok(_todos);
-});
+//app.MapGet("/todo",  async( [FromServices] ITodoService _todoservice,ILogger<Program> loger) =>
+//{
+//    var  _todos = await _todoservice.GetAllTodoAsync();
+//    loger.LogInformation("Hello Abdou Bienvenu sur les loggers");
+//    return Results.Ok(_todos);
+//});
 
-app.MapGet("/todo/{id:int}", async ([FromRoute] int id, [FromServices] ITodoService _todoservice) =>
-{
-    var _todo = await _todoservice.GetTodoByIDAsync(id);
-    if( _todo is not null) return  Results.Ok(_todo);
-    return Results.NotFound();
-});
+//app.MapGet("/todo/{id:int}", async ([FromRoute] int id, [FromServices] ITodoService _todoservice) =>
+//{
+//    var _todo = await _todoservice.GetTodoByIDAsync(id);
+//    if( _todo is not null) return  Results.Ok(_todo);
+//    return Results.NotFound();
+//});
 
 
-app.MapPost("/todo", (  
-    [FromBody]TodoInput input,
-    [FromServices] ITodoService service,
-    [FromServices] IValidator<TodoInput> validator ) =>
-{
-    var result = validator.Validate(input);
-    if(!result.IsValid)
-    {
-        return Results.BadRequest(result.Errors.Select(e => new
-        {
-            e.ErrorMessage,
-            e.PropertyName
-        }));
-    }
-    var _todo = service.AddTodoAsync(input);
-    return Results.Ok(_todo);   
-});
+//app.MapPost("/todo", (  
+//    [FromBody]TodoInput input,
+//    [FromServices] ITodoService service,
+//    [FromServices] IValidator<TodoInput> validator ) =>
+//{
+//    var result = validator.Validate(input);
+//    if(!result.IsValid)
+//    {
+//        return Results.BadRequest(result.Errors.Select(e => new
+//        {
+//            e.ErrorMessage,
+//            e.PropertyName
+//        }));
+//    }
+//    var _todo = service.AddTodoAsync(input);
+//    return Results.Ok(_todo);   
+//});
 
-app.MapPut("/todo/{id:int}", async (
-    [FromRoute]int id ,
-    [FromBody] TodoInput input,
-    [FromServices] IValidator<TodoInput> validator,
-    [FromServices] ITodoService service) =>
-{
-    var result = validator.Validate(input);
-    if(!result.IsValid)
-    {
-        return Results.BadRequest(result.Errors.Select(e => new
-        {
-            e.ErrorMessage,
-            e.PropertyName
-        }));
-    }
-    var _todo =  await service.UpdateTodoAsync( id,input);
-    return Results.Ok(_todo);
-});
+//app.MapPut("/todo/{id:int}", async (
+//    [FromRoute]int id ,
+//    [FromBody] TodoInput input,
+//    [FromServices] IValidator<TodoInput> validator,
+//    [FromServices] ITodoService service) =>
+//{
+//    var result = validator.Validate(input);
+//    if(!result.IsValid)
+//    {
+//        return Results.BadRequest(result.Errors.Select(e => new
+//        {
+//            e.ErrorMessage,
+//            e.PropertyName
+//        }));
+//    }
+//    var _todo =  await service.UpdateTodoAsync( id,input);
+//    return Results.Ok(_todo);
+//});
 
-app.MapDelete("/todo/{id:int}", 
-   async ([FromRoute] int id,[FromServices] ITodoService service) =>
-{
-    var result =  await service.DeleteTodoAsync(id);
-    if (result) return Results.Content("Supprimé!");
-    return Results.NoContent();
-});
+//app.MapDelete("/todo/{id:int}", 
+//   async ([FromRoute] int id,[FromServices] ITodoService service) =>
+//{
+//    var result =  await service.DeleteTodoAsync(id);
+//    if (result) return Results.Content("Supprimé!");
+//    return Results.NoContent();
+//});
 app.Run();
